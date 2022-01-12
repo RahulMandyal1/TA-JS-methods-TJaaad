3ee1b44bcd5f7a5c99c482ff9e3778d213928dc4
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-01-12 14:23:20 +0530

    completed array assignment

---
+++ block-BJaadg/code/index.js
@@ -0,0 +1,124 @@
+// After knowing about string methods, practice those by solving problems given below.
+
+let from = 'Syrio Forel';
+let quote = 'There  is only one thing we say to death: Not today';
+let to = 'Arya Stark';
+
+/*
+1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
+*/
+
+let indexOfIs = quote.indexOf("is");
+
+
+/*
+2. Find the character at the index indexOfIs (Problem 1) in quote.
+*/
+// dont get  the question
+// Refractor 
+quote.charAt(indexOfIs);
+/*
+3. Log the message saying `The index of first is in quote is 7`
+*/
+console.log(` the index of  first is  in  quote is ${indexOfIs}`);
+/*
+4. Log the message for first 6 characters of quote like this.
+  The character at index 0 is 'T'
+  The character at index 1 is 'h'
+  The character at index 2 is 'e'
+  The character at index 3 is 'r'
+  The character at index 4 is 'e'
+  The character at index 5 is ' '
+*/
+for(let i = 0; i <=5 ; i++){
+console.log(`The character at index ${i} is : ${quote[i] }`);
+}
+/*
+5. Using the variable from , to and quote variable dispaly this message
+  "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
+*/
+
+console.log(from.concat(" said ",quote));
+/*
+6. Does from, to and quote ends with "rk". Check all three.
+*/
+to.endsWith("rk"); // true yes it ends with  "rk"
+quote.endsWith("rk"); // flase // no  it does not  ends  with "rk"
+/*
+7. Does quote includes the word "Only"
+*/
+quote.includes("Only");// false // it does not  include Only  word
+/*
+8. Does quote includes the word " we"
+*/
+quote.includes("we"); //true // yes it  includes the  word we
+/*
+9. Find the index of the the word `we` in quote
+*/
+quote.indexOf("we");//25
+/*
+10. Split the quote into individual word and store it in a variable name quoteSplitted
+*/
+let  quoteSplitted =  quote.split(" ");
+
+
+/*
+11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
+*/
+
+quoteSplitted = quoteSplitted.join(" ");
+quoteSplitted  = quote.replace("today","tomorrow");
+
+
+
+
+/*
+12. Find the index of second "o" in quote. Use indexOf
+*/
+quote.indexOf("o"); //10
+/*
+13. Find the last index of letter "a" in quote.
+*/
+quote.lastIndexOf("a"); // 49
+/*
+14. Find the second last index of letter "a" in quote.
+*/
+quote.lastIndexOf("a" , 48); // 37
+/*
+15. Make the quote 70 character long. If it has less characters add rest as .......
+Example: "Hello" (convert to 10 characters) => "Hello....."
+Store the output in a new variable
+*/
+let quoteWithPaddingEnd =   quote.padEnd(70 ,"." );
+/*
+16. Do same as (15) but the ... should come in start. Store the output in a new variable
+*/
+let quoteWithPaddingStart = quote.padStart(66 , ".");
+
+/*
+17. Log the repeat of "Hello World!" 10 times.
+*/
+console.log("Hello World!".repeat(10));
+/*
+18. Replace today to tomorrow in quote.
+*/
+ quote = quote.replace("today", "tomorrow");
+/*
+19. Replace Stark to Lannister in quoteTo
+*/
+quoteTo  = to.replace("Stark" ,"Lannister");
+
+/*
+20. Make the quote of length 30 and put ... at the end. (use slice)
+*/
+quote= quote.slice(30);
+quote = quote.padEnd(30 , ".");
+
+/*
+21. Find out does quote, from, to starts with "A"
+*/
+
+// Code  Refractor
+console.log(quote.startsWith("A"));
+console.log(from.startsWith("A"));
+console.log(to.startsWith("A"));
--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -11,55 +11,104 @@ Clone the array before using sort method: [...arr]
 */
 
 // - Find the index of `101` in numbers
+console.log(numbers.indexOf(101));
 
 // - Find the last index of `9` in numbers
-
+console.log(numbers.lastIndexOf(9));
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+let newstrings  =  strings.join(" ");
 // - Add two new words in the strings array "called" and "sentance"
-
+strings.push("called","sentence");
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+let newstr = strings.join(" ");
 // - Remove the first word in the array (strings)
 
+strings.shift();
 // - Find all the words that contain 'is' use string method 'includes'
 
 // - Find all the words that contain 'is' use string method 'indexOf'
 
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
+numbers.every(element => {
+  element %3 == 0;
+}) ; // false all numbers  are not divisible by 3
 // -  Sort Array from smallest to largest
-
+let sortedNumbers =[...numbers].sort( function( a , b){
+  if(a > b) return 1;
+  if(a < b) return -1;
+  return 0;
+});
 // - Remove the last word in strings
-
+strings.pop();
 // - Find largest number in numbers
+
+let largest = numbers[0];
+
+for( let i = 0; i<=numbers.length ; i++){
+  if(largest <numbers[i]){
+    largest = numbers[i]
+  }
+}
+console.log(largest);
+
 
 // - Find longest string in strings
 
+let largestString = strings[0];
+
+for( let i = 0; i<=strings.length ; i++){
+  if(largestString < strings[i]){
+    largestStrings = strings[i]
+  }
+}
+console.log(largestString);
+
 // - Find all the even numbers
 
+ let evenNumbers =numbers.filter(element=> element %2==0);
+ console.log(evenNumbers);
+
 // - Find all the odd numbers
+let oddNumbers =numbers.filter(element=> {
+  return element %3==0;
+});
+console.log(oddNumbers);
 
 // - Place a new word at the start of the array use (unshift)
-
+strings.unshift("new word");
 // - Make a subset of numbers array [18,9,7,11]
-
+let numberSubset = numbers.slice(3 , 7);
+console.log(numberSubset);
 // - Make a subset of strings array ['a','collection']
-
+let stringSubset = strings.slice(2,4);
+console.log(stringSubset);
 // - Replace 12 & 18 with 1221 and 1881
+numbers[12] = 1221;
+numbers[18] = 1881;
 
 // - Replace words in strings array with the length of the word
-
+let strlen = 0;
+let  replaceStrLength = strings.map(element=> {
+   strlen = element.length ;
+   return  element = strlen;
+   
+});
 // - Find the sum of the length of words using above question
 
+let  sumOfArray = replaceStrLength.reduce((accu = initialValue , element ) =>{
+   return accu+=element ;
+}, initialValue = 0 );
 // - Customers Array
-var customers = [
+ var customers = [
   { firstname: 'Joe', lastname: 'Blogs' },
   { firstname: 'John', lastname: 'Smith' },
   { firstname: 'Dave', lastname: 'Jones' },
-  { firstname: 'Jack', lastname: 'White' },
+  { firstname: 'Jack', lastname: 'White' }
 ];
 // - Find all customers whose firstname starts with 'J'
+let firstNameWithJ =  filter(element =>{
+  //  return element.firstname.inc
+})
 
 // - Create new array with only first name
 
